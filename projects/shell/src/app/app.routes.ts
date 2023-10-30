bf0065053ac77bdbe51de8e541f8f07e521c9a22
Jaykumar Parmar <jaykumar.parmar@siemens.com>
2023-10-30 17:19:36 +0530

    non dynamic angular mfe

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,13 +1,6 @@
-import { loadRemoteModule } from '@angular-architects/module-federation';
-import {
-  WebComponentWrapper,
-  WebComponentWrapperOptions,
-} from '@angular-architects/module-federation-tools';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-
-const URL = 'http://localhost:3000/remoteEntry.js';
 
 export const APP_ROUTES: Routes = [
   {
